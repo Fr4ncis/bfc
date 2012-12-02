db4b51bb473265cff761d20cd7dc145d379601ba
Ivana Setiawan <ivana.setiawan@gmail.com>
2012-12-02 13:16:28 +0100

    icomoon

--- js/playsong.js
+++ js/playsong.js
@@ -102,7 +102,7 @@ function getSongs() {
              '<a href="javascript:playSong(\''+song.get('spotify_url')+'\')">'+
                  '<h3>'+song.get('artist')+'</h3>'+
                  '<p>'+song.get('title')+'</p>'+
-                 '<p>Played '+song.get('play_count')+' times, '+song.get('budget')+' plays remaining</p>'+
+                 '<p class="playcount icon-music">Played '+song.get('play_count')+' times, '+song.get('budget')+' plays remaining</p>'+
              '</a>'+
          '</li>');
       
