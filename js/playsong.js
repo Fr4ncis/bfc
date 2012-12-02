4d7f947a91654bf6c330f7bba5b98c3ea553b58a
Erik Romijn <eromijn@solidlinks.nl>
2012-12-02 13:07:11 +0100

    play count -> donation count

--- js/playsong.js
+++ js/playsong.js
@@ -102,7 +102,7 @@ function getSongs() {
              '<a href="javascript:playSong(\''+song.get('spotify_url')+'\')">'+
                  '<h3>'+song.get('artist')+'</h3>'+
                  '<p>'+song.get('title')+'</p>'+
-                 '<p>Played '+song.get('play_count')+' times, '+song.get('budget')+' plays remaining</p>'+
+                 '<p>This song generated € '+(song.get('play_count')/10).toFixed(2)+' in donations, '+song.get('budget')+' plays remaining</p>'+
              '</a>'+
          '</li>');
       
